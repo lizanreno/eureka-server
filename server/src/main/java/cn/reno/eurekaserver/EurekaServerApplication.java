831643819df8bdcb310f73843714b4509d6746d1
lizana <lizana@enn.cn>
2020-05-12 22:27:54 +0800

    【SpringCloud SetUp】organize code package

--- server/src/main/java/cn/reno/eurekaserver/EurekaServerApplication.java
+++ server/src/main/java/cn/reno/eurekaserver/EurekaServerApplication.java
@@ -1,6 +1,5 @@
 package cn.reno.eurekaserver;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
